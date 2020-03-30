59566f91fc31830adee0397becb2fa1a96c175e8
Kaleb Scholes <kscholes@gmail.com>
2020-03-29 21:45:17 -0600

    Update GA Code

--- src/analytics.js
+++ src/analytics.js
@@ -2,5 +2,6 @@ import ReactGA from 'react-ga'
 
 const trackerId = process.env.APP_GA_TRACKING_ID
 ReactGA.initialize(trackerId)
+ReactGA.pageview(window.location.pathname + window.location.search)
 
 export default ReactGA
